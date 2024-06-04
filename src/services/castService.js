116550ce0ce0952fc789b404b18ed1a0738c1a78
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-06-04 15:52:08 +0300

    .

--- src/services/castService.js
+++ src/services/castService.js
@@ -1,4 +1,9 @@
 const Cast = require(`../models/Cast`);
+const Movie = require(`../models/Movie`);
 
 exports.getAll = () => Cast.find();
 exports.create = (castData) => Cast.create(castData);
+exports.getByMovieId =  async (movieId) => {
+    const movie = await Movie.findById(movieId);
+    const casts = await Cast.find({_id})
+}
